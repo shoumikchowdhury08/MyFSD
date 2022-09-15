9c92cb57711ad8a713a502839f9d60b3cac149a5
shoumikchowdhury08 <shoumikchowdhury08@gmail.com>
2022-09-15 13:34:53 +0530

    reverse a string

--- src/Course3_Module2/appleReverse.java
+++ src/Course3_Module2/appleReverse.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class appleReverse {
     public static String reverse(String str) {
 
-        if (str == null || str.length()<=1){
+        if (str == null || str.length() <= 1) {
             return str;
-        }
-        else{
-            return reverse(str.substring(1))+str.charAt(0);
+        } else {
+            return reverse(str.substring(1)) + str.charAt(0);
         }
 
     }
 
-    public static void main(String args[] ) throws Exception {
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         System.out.println(reverse(str));
